84b15ebe3f5d45946bbfd6edcab27ea7a6504de5
Mi-lex <mishin-lex@mail.ru>
2021-06-19 22:40:04 +0300

    Allow tsconfig

--- core/.eslintrc.js
+++ core/.eslintrc.js
@@ -31,6 +31,7 @@ module.exports = {
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
         'no-console': 'error',
         complexity: ['error', 8],
         'default-param-last': ['error'],
